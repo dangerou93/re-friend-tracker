4588c35667e9dac8466f0f4defb81d02f583f371
dangerou93 <pmoesch93@hotmail.com>
2018-12-16 11:53:43 +0100

    updated

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -139,7 +139,7 @@ export class GuiModel {
                         {
                             "type": "okButton",
                             "name": "Ok"
-                        }
+                        },
                     ]
                 },
 				{
