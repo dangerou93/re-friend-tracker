610e9ced7f01291c50399d7d0732d4553860e44e
dangerou93 <pmoesch93@hotmail.com>
2018-12-16 11:37:50 +0100

    updated

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,13 +6,14 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Patrick's Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
                     "title": "Friend",
                     "url": "/friend",
                     "formFieldList": [
+			
                         {
                             "id": "familyName",
                             "type": "text",
@@ -27,6 +28,20 @@ export class GuiModel {
                             "width": 1,
                             "required": true
                         },
+						{ 	"id": "nickname", 
+							"type": "text",
+							"name": "Nickname", 
+							"width": 2, 
+							"required": true
+						},
+						{
+							"id": "group",
+							"type": "autocomplete",
+							"name": "Group",
+							"url": "/group",
+							"form": "GroupForm",
+							"width": 2
+						},
                         {
                             "id":   "location",
                             "type": "autocomplete",
@@ -105,6 +120,15 @@ export class GuiModel {
                             "width": 2
                         },
                         {
+                            "id": "location",
+                            "type": "text",
+                            "name": "Location",
+							"defaultKey": "locationKey"
+							"form": "LocationForm",
+                            "width": 2,
+                            "required": true
+                        },
+                        {
                             "type": "deleteButton",
                             "name": "Delete"
                         },
@@ -118,6 +142,33 @@ export class GuiModel {
                         }
                     ]
                 },
+				{
+					"id": "GroupForm":
+					"title": "Group",
+					"url": "/group",
+					"formFieldList":[
+					{
+						"id": "name",
+						"type": "text",
+						"name": "GroupName",
+						"width": 2,
+						"required": true
+					},
+					{	
+						"type": "deleteButton",
+						"name": "Delete"
+					},
+					{
+						"type": "cancelButton",
+						"name": "Cancel",
+					},
+					{
+						"type": "okButton",
+						"name": "Ok"
+					}
+				]
+				}
+					
             ],
             "pageList": [
                 {
@@ -138,6 +189,20 @@ export class GuiModel {
                             "color": "yellow",
                             "page": "locationspage",
                         },
+						{
+							"type": "button",
+							"name": "Groups",
+							"icon": "fa-weixin",
+							"color": "wisteria",
+							"page": "groupspage",
+						},
+						{
+							"type": "button",
+							"name": "Activity",
+							"icon": "fa-bicycle",
+							"color": "midnight-blue",
+							"page": "activitypage",
+						},
                     ]
                 },
                 {
@@ -195,6 +260,65 @@ export class GuiModel {
                         },
                     ]
                 },
+				{
+					"id": "groupspage",
+					"elementList":[
+					
+					{
+						"type": "backbutton",
+					},
+					{
+						"type": "newButton",
+						"name": "NewGroup",
+						"icon": "fa-weixin",
+						"color": "green",
+						"form": {
+							"form": "GroupForm"
+						}
+					},
+					{
+						"type": "list",
+						"icon": "fa-weixin",
+						"color": "wisteria",
+						"search": true,
+						"url": "/group",
+						"form":{
+							"form": "GroupForm"
+						}
+					},
+					]
+
+				},
+				{
+					"id": "activitypage",
+					"elementList":[
+					
+					{
+						"type": "backbutton",
+					},
+					{
+						"type": "newButton",
+						"name": "NewActivity",
+						"icon": "fa-bicycle",
+						"color": "green",
+						"form": {
+							"form": "AddActivityForm"
+						}
+					},
+					{
+						"type": "list",
+						"icon": "fa-bicycle",
+						"color": "midnight-blue",
+						"search": true,
+						"url": "/activity",
+						"form":{
+							"form": "AddActivityForm"
+						}
+					},
+					]
+				
+				},
+						
             ]
         }
     };
