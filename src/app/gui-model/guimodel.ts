39a7b52cbd0cdcf2fe7e5b0f3bdf0f245b792845
dangerou93 <pmoesch93@hotmail.com>
2018-12-19 23:56:41 +0100

    updated

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -277,7 +277,7 @@ export class GuiModel {
                             "search": true,
                             "url": "/friend",
                             "page" "friendpage"
-                            }
+                          
                         },
                     ]
                 },
